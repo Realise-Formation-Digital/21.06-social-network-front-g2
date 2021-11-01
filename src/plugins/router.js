5ab0898d856327ff451029573fb6c67df4640fd0
Marco Tribuzio <marco.tribuz@gmail.com>
2021-11-01 12:22:28 +0100

    First Commit

---
+++ src/plugins/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from "../pages/Home";
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/', component: Home
+}]
+
+export default new VueRouter({
+    routes: routes // short for `routes: routes`
+})
+
