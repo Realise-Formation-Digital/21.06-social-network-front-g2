bb91aa566ac6242c54d7e6557e3cd98d1deeeb0d
Martinez <martinez_david@me.com>
2021-11-02 10:09:13 +0100

    First commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- Header with button. David-->
+  <mdb-navbar expand="large" dark color="indigo">
+    <mdb-navbar-brand href="#">
+      ADL Gaming
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <mdb-nav-item to="/">Home</mdb-nav-item>
+        <mdb-nav-item to="/result">Result</mdb-nav-item>
+        <mdb-nav-item to="/contact">Contact</mdb-nav-item>
+        <mdb-nav-item to="/about">About</mdb-nav-item>
+        <mdb-nav-item to="/recherche">Rechercher</mdb-nav-item>
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+</template>
+
+<!-- Script components. David-->
+<script>
+  import {  mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem } from 'mdbvue';
+  export default {
+    name: 'Pills',
+    components: {
+      mdbNavbar,
+      mdbNavbarBrand,
+      mdbNavbarToggler,
+      mdbNavbarNav,
+      mdbNavItem,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
