1ced1780a7050102c729bcaf0a1cf8185bb7e9aa
Marco Tribuzio <marco.tribuz@gmail.com>
2021-11-09 09:16:18 +0100

    Done ;)

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -3,28 +3,32 @@
     <b-row class="">
       <b-col md="0" lg="3"></b-col>
       <b-col class="name"
-        ><div class="file">
+      >
+        <div class="file">
           <form enctype="multipart/from-data">
             <div class="fields">
               <b-form-input v-model="title" placeholder="Title"></b-form-input
-              ><br />
+              >
+              <br/>
               <b-form-textarea
-                id="textarea-rows"
-                placeholder="Contents"
-                rows="8"
-                v-model="content"
+                  id="textarea-rows"
+                  placeholder="Contents"
+                  rows="8"
+                  v-model="content"
               ></b-form-textarea
-              ><br />
+              >
+              <br/>
               <input
-                type="file"
-                accept="image/*"
-                id="file-input"
-                @change="uploadImage($event)"
+                  type="file"
+                  accept="image/*"
+                  id="file-input"
+                  @change="uploadImage($event)"
               />
             </div>
           </form>
           <b-button pill @click="newUser">Send</b-button>
-        </div></b-col
+        </div>
+      </b-col
       >
       <b-col md="0" lg="3"></b-col>
     </b-row>
@@ -38,7 +42,7 @@ export default {
 
   data() {
     return {
-      image: null,
+      base64Image: null,
       title: "",
       content: "",
     };
@@ -47,12 +51,10 @@ export default {
   methods: {
 
     newUser() {
-      let img64 = this.getBase64(this.image);
-      console.log(img64);
       let json = {
         title: this.title,
         content: this.content,
-        img: img64,
+        img: this.base64Image && this.base64Image.base64Image,
         author: "placeholder",
         date: Date.now(),
         likes: 1,
@@ -61,22 +63,21 @@ export default {
     },
 
     uploadImage(event) {
-      this.image = event.target.files[0];
-      console.log(this.image);
-    },
-
-    getBase64(file) {
+      const uploadedImage = event.target.files[0];
+      let image = {
+        base64Image: null
+      }
       let reader = new FileReader();
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(uploadedImage);
       reader.onload = function () {
-        console.log(reader.result);
+        image.base64Image = reader.result
       };
       reader.onerror = function (error) {
         console.log("Error: ", error);
       };
-      return reader.result;
-    },
-    
+
+      this.base64Image = image
+    }
   },
 };
 </script>
@@ -88,9 +89,11 @@ export default {
   margin-top: 10vh;
   padding-bottom: 10px;
 }
+
 .button-color {
   background-color: cornflowerblue;
 }
+
 .file {
   height: 400px;
 }
